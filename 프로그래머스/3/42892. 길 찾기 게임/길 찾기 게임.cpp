74d947336e2aa719e7a76cfd71594f59344953be
pulbak <81692085+parkhyeok11@users.noreply.github.com>
2024-05-24 16:57:35 +0900

    [level 3] Title: 길 찾기 게임, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
@@ -5,14 +5,12 @@
 using namespace std;
 
 struct Node {
-    Node* left;
-    Node* right;
+    Node* left, * right;
     int x;
     int y;
     int idx;
 };
 
-// 비교 함수 수정: y 기준 내림차순, y가 같으면 x 기준 오름차순
 bool cmp(Node a, Node b) {
     if (a.y == b.y) {
         return a.x < b.x;
@@ -20,24 +18,25 @@ bool cmp(Node a, Node b) {
     return a.y > b.y;
 }
 
-// 이진 트리 구성 함수 수정
 void Make_Binary_Tree(Node* Root, Node* Child) {
     if (Child->x < Root->x) {
         if (Root->left == NULL) {
             Root->left = Child;
-        } else {
+        }
+        else {
             Make_Binary_Tree(Root->left, Child);
         }
-    } else {
+    }
+    else {
         if (Root->right == NULL) {
             Root->right = Child;
-        } else {
+        }
+        else {
             Make_Binary_Tree(Root->right, Child);
         }
     }
 }
 
-// 전위 순회
 void preorder(Node* Root, vector<int>& answer) {
     if (Root == NULL) return;
     answer.push_back(Root->idx);
@@ -45,7 +44,6 @@ void preorder(Node* Root, vector<int>& answer) {
     preorder(Root->right, answer);
 }
 
-// 후위 순회
 void postorder(Node* Root, vector<int>& answer) {
     if (Root == NULL) return;
     postorder(Root->left, answer);
@@ -55,31 +53,28 @@ void postorder(Node* Root, vector<int>& answer) {
 
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
     vector<vector<int>> answer;
-
-    // Node 벡터 생성 및 인덱스 저장
-    vector<Node> tree;
+    // 인덱스를 기억하기 위해 (값, 인덱스) 쌍으로 저장
+    vector<pair<vector<int>, int>> nodeinfo_with_index;
     for (int i = 0; i < nodeinfo.size(); ++i) {
-        tree.push_back({ NULL, NULL, nodeinfo[i][0], nodeinfo[i][1], i + 1 });
+        nodeinfo_with_index.push_back({ nodeinfo[i], i });
     }
 
-    // 노드 정렬
-    sort(tree.begin(), tree.end(), cmp);
+    vector<Node> tree;
 
-    // 이진 트리 구성
+    for (const auto& node : nodeinfo_with_index) {
+        int x = node.first[0];
+        int y = node.first[1];
+        int idx = node.second+1;
+        tree.push_back({ NULL,NULL,x,y,idx });
+    }
+    sort(tree.begin(),tree.end(), cmp);
     Node* Root = &tree[0];
-    for (int i = 1; i < tree.size(); ++i) {
-        Make_Binary_Tree(Root, &tree[i]);
-    }
-
-    // 전위 순회 및 후위 순회
+    for (int i = 1; i < tree.size(); i++) Make_Binary_Tree(Root, &tree[i]);
     vector<int> pre;
     preorder(Root, pre);
     vector<int> post;
     postorder(Root, post);
-
-    // 결과 저장
     answer.push_back(pre);
     answer.push_back(post);
-
     return answer;
 }
